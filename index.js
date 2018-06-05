d29701e6a798a21ca994174eade6cee632f3ce15
robert.zacchia@gmail.com <robert.zacchia@gmail.com>
2018-06-05 18:52:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+var name = "Susan";
+var height = "74";
+
+// Don´t worry about this
+module.exports = {name, height};
+
+if (name === "Susan")
+{
+  console.log("the name is correct");
+}
+else
+{
+  console.log("Expected the name to be Susan");
+}

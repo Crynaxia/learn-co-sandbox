c09dfc5a4d468a422cd8870e26c7424f9682ea89
robert.zacchia@gmail.com <robert.zacchia@gmail.com>
2018-07-03 18:47:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-var name = "Susan";
-var height = "74";
-
-// Don´t worry about this
-module.exports = {name, height};
-
-if (name === "Susan")
-{
-  console.log("the name is correct");
-}
-else
-{
-  console.log("Expected the name to be Susan");
-}
+var element = document.createElement('div')
